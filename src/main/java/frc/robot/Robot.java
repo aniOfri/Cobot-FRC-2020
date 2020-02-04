a67f0ee6c3c1515865a43c6082e678ecd20bf1e1
Ofri <ofri.gutman@outlook.co.il>
2020-02-04 18:15:46 +0200

    Added TalonSRX and Climbing fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,30 +8,23 @@
 package frc.robot;
 
 // Import Essentials
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.VictorSP;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 
 
 public class Robot extends TimedRobot {
-  Joystick stick = new Joystick(0);
-  /*VictorSPX V1 = new VictorSPX(0);
-  VictorSPX V2 = new VictorSPX(1);
-  VictorSPX V3 = new VictorSPX(2);
-  VictorSPX V4 = new VictorSPX(3);
-
-  SpeedControllerGroup Left = new SpeedControllerGroup(V1,V2);
-  SpeedControllerGroup Right = new SpeedControllerGroup(V3,V4);
-  DifferentialDrive drive = new DifferentialDrive(Left, Right);*/
-
+  Joystick stick;
   VictorSPX[] motors;
-  VictorSP[] climb;
-
   //VictorSPX[] shooter;
+  TalonSRX[] climb;
+  VictorSP balance;
 
   /*
     Initialize all variables at deploy.
@@ -55,97 +48,71 @@ public class Robot extends TimedRobot {
     stick = new Joystick(0);
 
     
-    // Initialize motors as a two-item array
-    motors = new VictorSPX[7];
-    // Set item one as motor one
+    // Initialize motors as a four-item array
+    motors = new VictorSPX[4];
+    // Initialization
     motors[0] = new VictorSPX(0);
-    // Set item two as motor two
     motors[1] = new VictorSPX(1);
-    // Set item three as motor three
     motors[2] = new VictorSPX(2);
-    // Set item four as motor four
     motors[3] = new VictorSPX(3);
-    // Set item four as camera motor
-    //motors[4] = new VictorSP(4);
     
-    climb = new VictorSP[3];
-    climb[0] = new VictorSP(0);
-    climb[1] = new VictorSP(1);
-    climb[2] = new VictorSP(2);
+    // Initialize climb as a three-item array
+    climb = new TalonSRX[2];
+    // Initialization
+    climb[0] = new TalonSRX(0);
+    climb[1] = new TalonSRX(1);
+    balance = new VictorSP(0);
 
-    /* Initialize motors as a two-item array 
-    for shooting mechanism
-
-    // Declaration
+    /* 
+    //Initialize shooter as a two-item array 
     shooter = new VictorSP[2];
     // Initialization
     shooter[0] = new VictorSP(5);
     shooter[1] = new VictorSP(6);*/
     }
-  
-  /*
-    Periodic function which triggers repeatedly
-    while the teleOperated state is enabled.
 
-    @param throttle: double class which stores
-      the state of the Joystick's throttle. 
-  */
   @Override
   public void robotPeriodic() {
-    double throttle;
-    boolean trigger;
-
     // Movement
     movement();
 
-    // Get the Joystick's throttle's state.
-    throttle = stick.getThrottle();
-    trigger = stick.getTrigger();
-
-    //drive.arcadeDrive(stick.getRawAxis(1),stick.getRawAxis(0));
-    
-    if(stick.getRawButton(5)){
-      climb[0].set(0.5);
-      climb[1].set(0.5);
+    if(stick.getRawButton(4)){
+      climb[0].set(ControlMode.PercentOutput, 0.5);
+      climb[1].set(ControlMode.PercentOutput, 0.5);
     }
-    else if(stick.getRawButton(3)){
-      climb[0].set(-0.5);
-      climb[1].set(-0.5);
+    else if(stick.getRawButton(4) && stick.getTop()){
+      climb[0].set(ControlMode.PercentOutput, -0.5);
+      climb[1].set(ControlMode.PercentOutput, -0.5);
     }
     else{
-      climb[0].set(0);
-      climb[1].set(0);
+      climb[0].set(ControlMode.PercentOutput, 0);
+      climb[1].set(ControlMode.PercentOutput, 0);
     }
 
-    if(stick.getRawButton(6)){
-      climb[2].set(1);
-    }
-    else if(stick.getRawButton(4)){
-      climb[2].set(-1);
-    }
-    else{
-      climb[2].set(0);
-     
-    }
+    if(stick.getRawButton(4))
+      balance.set(1);
+    else if(stick.getRawButton(4) && stick.getTop())
+      balance.set(-1);
+    else
+      balance.set(0);
+
     // SmartDashboard
-    SmartDashboard.putNumber("Throttle: ", throttle);
-    SmartDashboard.putBoolean("Trigger: ", trigger);
-
+    SmartDashboard.putBoolean("Trigger: ", stick.getTrigger());
 
     /*
     // If trigger is pressed
     if (stick.getTrigger()){
-      motors[2].set(-throttle);
-      //shooter[1].set(throttle);
+      shooter[0].set(1);
+      shooter[1].set(1);
     }
     // Else, set the motors to 0
     else{
-      motors[2].set(0);
-      //shooter[1].set(0);
+      shooter[0].set(0);
+      shooter[1].set(0);
     }*/
     }
 
-    public void movement(){
+  public void movement(){
         // Declare variables
         double maxLim,
         sqrt,
@@ -163,7 +130,7 @@ public class Robot extends TimedRobot {
             int negFix = yVal < -0.2 ? -1 : 1,
                 negPos = (int)(yVal / Math.abs(yVal));
 
-            maxLim = 0.7 + (-stick.getThrottle()  + 1) / 2 * 0.3;
+            maxLim = 0.85;
             sqrt = Math.sqrt(xVal * xVal + yVal * yVal);
 
             finalL = sqrt * maxLim * negPos;
@@ -178,10 +145,6 @@ public class Robot extends TimedRobot {
         SmartDashboard.putNumber("FinalL", finalL);
 
         // Sending values to motor(0), motor(1)
-        
-        SmartDashboard.putNumber("finalR", finalR);
-        SmartDashboard.putNumber("finalL", finalL);
-
         motors[0].set(ControlMode.PercentOutput, finalR);
         motors[1].set(ControlMode.PercentOutput, finalR);
         motors[2].set(ControlMode.PercentOutput, finalL);
